f9ae157322d89756ef5f99e6348dcc3f8c66a26b
Charitha Weerasooriya <charithaw1234@gmail.com>
2020-02-27 14:31:19 +0530

    heroku settings

--- ClothClassificationBackend/settings.py
+++ ClothClassificationBackend/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
-django_heroku.settings(locals())
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -121,3 +121,4 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+django_heroku.settings(locals())
